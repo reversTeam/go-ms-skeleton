bfbbf6aa2765eaf73a302de35153b244923f5c15
Théotime Rivière <t@ffs.holdings>
2024-02-15 04:04:48 +0100

    clean useless services

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
 
-	"github.com/reversTeam/go-ms-skeleton/services/ping"
 	"github.com/reversTeam/go-ms-tools/middlewares"
-	"github.com/reversTeam/go-ms-tools/services/abs"
 	"github.com/reversTeam/go-ms-tools/services/account"
-	"github.com/reversTeam/go-ms-tools/services/child"
 	"github.com/reversTeam/go-ms-tools/services/email"
 	"github.com/reversTeam/go-ms-tools/services/people"
 	"github.com/reversTeam/go-ms-tools/services/signin"
@@ -25,26 +20,7 @@ var (
 	configFilePath = flag.String("config", GO_MS_CONFIG_FILEPATH, "yaml config filepath")
 )
 
-type TestMiddleware struct {
-	core.BaseMiddleware
-}
-
-func (t *TestMiddleware) Apply(ctx context.Context, req interface{}) (context.Context, interface{}, error) {
-	fmt.Println("TEST MIDDLEWARE IS APPLIED")
-
-	return ctx, req, nil
-}
-
 var goMsServices = map[string]core.GoMsServiceFunc{
-	"abs": core.RegisterServiceMap(func(ctx *core.Context, name string, config core.ServiceConfig) core.GoMsServiceInterface {
-		return abs.NewService(ctx, name, config)
-	}),
-	"child": core.RegisterServiceMap(func(ctx *core.Context, name string, config core.ServiceConfig) core.GoMsServiceInterface {
-		return child.NewService(ctx, name, config)
-	}),
-	"ping": core.RegisterServiceMap(func(ctx *core.Context, name string, config core.ServiceConfig) core.GoMsServiceInterface {
-		return ping.NewService(ctx, name, config)
-	}),
 	"people": core.RegisterServiceMap(func(ctx *core.Context, name string, config core.ServiceConfig) core.GoMsServiceInterface {
 		return people.NewService(ctx, name, config)
 	}),
@@ -61,7 +37,6 @@ var goMsServices = map[string]core.GoMsServiceFunc{
 var goMsMiddleWare = map[string]core.Middleware{
 	"AuthMiddleware":   &middlewares.AuthMiddleware{},
 	"UnAuthMiddleware": &middlewares.UnauthenticatedMiddleware{},
-	"TestMiddleware":   &TestMiddleware{},
 }
 
 func main() {
